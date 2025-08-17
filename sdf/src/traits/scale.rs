80715c665ce50a89268897fc2a0274046b78d99e
Alex Colby <alexdcolby@pm.me>
2025-08-17 16:26:07 +0100

    tweak(sdf): precomputes components used in scaling SDFs
    This lets us avoid a bit of computation at call time

--- sdf/src/traits/scale.rs
+++ sdf/src/traits/scale.rs
@@ -3,30 +3,35 @@ use marchrs_vectors::Vector;
 
 pub trait SdfScale<const N: usize>: Sdf<N> + Sized {
   fn scale(self, scale: impl Into<Vector<N>>) -> Scale<N, Self> {
-    Scale(self, scale.into())
+    let scale = scale.into();
+    Scale(self, 1.0 / scale, scale.min())
   }
 }
 
 impl<const N: usize, T: Sdf<N>> SdfScale<N> for T {}
 
+/// A Scaled `SDF` item `Scale(item, iscale, min)`
+/// - `item`: the `SDF` item to apply scaling to
+/// - `iscale`: `1.0 / scale`, precomputed for speed
+/// - `min`: the minimum axis of `scale`, precomputed for speed
 #[derive(Clone, Copy, PartialEq)]
-pub struct Scale<const N: usize, T>(T, Vector<N>);
+pub struct Scale<const N: usize, T>(pub T, Vector<N>, f64);
 
 impl<const N: usize, T: Default> Default for Scale<N, T> {
   fn default() -> Self {
-    Self(T::default(), Vector::ones())
+    Self(T::default(), Vector([1.0; N]), 1.0)
   }
 }
 
 impl<const N: usize, T: Sdf<N>> Sdf<N> for Scale<N, T> {
   #[inline]
   fn call(&self, pos: Vector<N>) -> f64 {
-    self.0.call(pos / self.1) * self.1.min()
+    self.0.call(pos * self.1) * self.2
   }
 
   #[inline]
   fn hits(&self, pos: Vector<N>) -> bool {
-    self.0.hits(pos / self.1)
+    self.0.hits(pos * self.1)
   }
 }
 
@@ -34,13 +39,13 @@ impl<const N: usize, T: SdfInfo<N>> SdfInfo<N> for Scale<N, T> {
   type Info = T::Info;
   #[inline]
   fn call_info(&self, pos: Vector<N>) -> (f64, Self::Info) {
-    self.0.call_info(pos / self.1)
+    self.0.call_info(pos * self.1)
   }
 }
 
 impl<const N: usize, T: SdfGrad<N>> SdfGrad<N> for Scale<N, T> {
   #[inline]
   fn call_grad(&self, pos: Vector<N>) -> (f64, Vector<N>) {
-    self.0.call_grad(pos / self.1)
+    self.0.call_grad(pos * self.1)
   }
 }
