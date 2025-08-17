98697b739432d29f3e4f0889245824f6be21d6d5
Alex Colby <alexdcolby@pm.me>
2025-08-17 15:39:35 +0100

    tweak: avoids unnecessary `map` and `zip_with` methods on `Vector`s
    This avoids unnecessary indirection and may
    be more transparent to the compiler

--- vectors/src/operators.rs
+++ vectors/src/operators.rs
@@ -1,111 +1,106 @@
 use super::Vector;
 use std::ops::{Add, Div, Mul, Neg, Sub};
 
-impl<const N: usize> Vector<N> {
-  /// Helper method to map over a vector<br>
-  /// used to make unary operators and binary operators with a float simpler.
-  fn map(self, f: impl Fn(f64) -> f64) -> Self {
-    Self(self.0.map(f))
-  }
-
-  /// Helper method to zip 2 vectors with a given function<br>
-  /// used to make binary operators vastly simpler to implement.
-  fn zip_with(mut self, rhs: Self, f: impl Fn(f64, f64) -> f64) -> Self {
-    for (lhs, rhs) in self.iter_mut().zip(rhs) {
-      *lhs = f(*lhs, rhs)
-    }
-    self
-  }
-}
-
 impl<const N: usize> Neg for Vector<N> {
   type Output = Vector<N>;
   fn neg(self) -> Self::Output {
-    self.map(Neg::neg)
+    Self(self.0.map(|x| -x))
   }
 }
 
 impl<const N: usize> Add<f64> for Vector<N> {
   type Output = Vector<N>;
   fn add(self, rhs: f64) -> Self::Output {
-    self.map(|x| x + rhs)
+    Self(self.0.map(|l| l + rhs))
   }
 }
 
 impl<const N: usize> Sub<f64> for Vector<N> {
   type Output = Vector<N>;
   fn sub(self, rhs: f64) -> Self::Output {
-    self.map(|x| x - rhs)
+    Self(self.0.map(|l| l - rhs))
   }
 }
 
 impl<const N: usize> Mul<f64> for Vector<N> {
   type Output = Vector<N>;
   fn mul(self, rhs: f64) -> Self::Output {
-    self.map(|x| x * rhs)
+    Self(self.0.map(|l| l * rhs))
   }
 }
 
 impl<const N: usize> Div<f64> for Vector<N> {
   type Output = Vector<N>;
   fn div(self, rhs: f64) -> Self::Output {
-    self.map(|x| x / rhs)
+    Self(self.0.map(|l| l / rhs))
   }
 }
 
 impl<const N: usize> Add<Vector<N>> for f64 {
   type Output = Vector<N>;
   fn add(self, rhs: Vector<N>) -> Self::Output {
-    rhs.map(|x| self + x)
+    Vector(rhs.0.map(|r| self + r))
   }
 }
 
 impl<const N: usize> Sub<Vector<N>> for f64 {
   type Output = Vector<N>;
   fn sub(self, rhs: Vector<N>) -> Self::Output {
-    rhs.map(|x| self - x)
+    Vector(rhs.0.map(|r| self - r))
   }
 }
 
 impl<const N: usize> Mul<Vector<N>> for f64 {
   type Output = Vector<N>;
   fn mul(self, rhs: Vector<N>) -> Self::Output {
-    rhs.map(|x| self * x)
+    Vector(rhs.0.map(|r| self * r))
   }
 }
 
 impl<const N: usize> Div<Vector<N>> for f64 {
   type Output = Vector<N>;
   fn div(self, rhs: Vector<N>) -> Self::Output {
-    rhs.map(|x| self / x)
+    Vector(rhs.0.map(|r| self / r))
   }
 }
 
 impl<const N: usize> Add<Vector<N>> for Vector<N> {
   type Output = Vector<N>;
-  fn add(self, rhs: Vector<N>) -> Self::Output {
-    self.zip_with(rhs, Add::add)
+  fn add(mut self, rhs: Vector<N>) -> Self::Output {
+    for (mut_l, r) in self.iter_mut().zip(rhs) {
+      *mut_l += r;
+    }
+    self
   }
 }
 
 impl<const N: usize> Sub<Vector<N>> for Vector<N> {
   type Output = Vector<N>;
-  fn sub(self, rhs: Vector<N>) -> Self::Output {
-    self.zip_with(rhs, Sub::sub)
+  fn sub(mut self, rhs: Vector<N>) -> Self::Output {
+    for (mut_l, r) in self.iter_mut().zip(rhs) {
+      *mut_l -= r;
+    }
+    self
   }
 }
 
 impl<const N: usize> Mul<Vector<N>> for Vector<N> {
   type Output = Vector<N>;
-  fn mul(self, rhs: Vector<N>) -> Self::Output {
-    self.zip_with(rhs, Mul::mul)
+  fn mul(mut self, rhs: Vector<N>) -> Self::Output {
+    for (mut_l, r) in self.iter_mut().zip(rhs) {
+      *mut_l *= r;
+    }
+    self
   }
 }
 
 impl<const N: usize> Div<Vector<N>> for Vector<N> {
   type Output = Vector<N>;
-  fn div(self, rhs: Vector<N>) -> Self::Output {
-    self.zip_with(rhs, Div::div)
+  fn div(mut self, rhs: Vector<N>) -> Self::Output {
+    for (mut_l, r) in self.iter_mut().zip(rhs) {
+      *mut_l /= r;
+    }
+    self
   }
 }
 
