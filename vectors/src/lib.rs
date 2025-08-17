e31846bbe396f1fcb84714a37c3bb3549b4e87e3
Alex Colby <alexdcolby@pm.me>
2025-08-17 15:34:45 +0100

    tweak(vec): provides a `mag2` method on vectors
    This lets some operations avoid a float `sqrt`
    when it isn't absolutely necessary

--- vectors/src/lib.rs
+++ vectors/src/lib.rs
@@ -72,16 +72,24 @@ impl<const N: usize> Vector<N> {
     Self(self.0.map(f64::abs))
   }
 
+  /// Magnitude squared of a vector.<br>
+  /// Using this method allows us to avoid a `sqrt`, which are among the
+  /// slowest CPU operations ([source](https://stackoverflow.com/a/12304868/14054238))
+  #[inline]
+  pub fn mag2(self) -> f64 {
+    self.into_iter().map(|x| x * x).sum()
+  }
+
   /// Magnitude of a vector
   #[inline]
   pub fn mag(self) -> f64 {
-    self.iter().map(|x| x * x).sum::<f64>().sqrt()
+    self.mag2().sqrt()
   }
 
   /// Normalises a vector
   #[inline]
   pub fn normal(self) -> Self {
-    self / self.mag()
+    self * self.mag().recip()
   }
 
   /// Dot Product
